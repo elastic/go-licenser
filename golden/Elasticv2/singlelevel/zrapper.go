344ac8a0a1ba7fdcec080792043ce24dbe8756cc
kruskal <99559985+kruskall@users.noreply.github.com>
2022-08-16 11:10:13 +0200

    lint: run go fmt

--- golden/Elasticv2/singlelevel/zrapper.go
+++ golden/Elasticv2/singlelevel/zrapper.go
@@ -2,6 +2,7 @@
 // or more contributor license agreements. Licensed under the Elastic License 2.0;
 // you may not use this file except in compliance with the Elastic License 2.0.
 
+//go:build mytag
 // +build mytag
 
 package testdata
